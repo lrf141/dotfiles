4e7f3b8a4233c0fa92d05ebd1b38c6cc26f0b9db
takuzoo <sata3868.k.serpentis@gmail.com>
2018-09-16 18:29:00 +0900

    :fire: remove unused NEO_BUNDLE script!!! Hahahahahaha

--- neobundle_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-sh ./install.sh
-rm -rf install.sh
-
-mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
